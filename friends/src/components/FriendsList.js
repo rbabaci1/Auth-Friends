50e833e4b31d80fd211736ad08a8b24be95629d4
rabah babaci <4rabah@gmail.com>
2020-04-25 16:05:29 -0700

    render h1 instead of anchor when in home page

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
+import { MDBIcon } from 'mdbreact';
 
 export default function FriendsList() {
+  // problem on page refresh
   return (
     <div>
+      {/* <MDBIcon icon='arrow-left' onClick={() => <Redirect path='/' />} /> */}
+
       <h1>You are logged In!</h1>
     </div>
   );
--- friends/src/components/Navbar.js
+++ friends/src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar({ history }) {
 
   return (
     <div className='nav-bar'>
-      {isLoggedIn ? (
+      {isLoggedIn || history.location.pathname === '/' ? (
         <h1 id='no-hover'>Welcome Friends!</h1>
       ) : (
         <Link to='/'>
