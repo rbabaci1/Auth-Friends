2a2ff12d54f4b3c6a8be86744081d38680ea004a
rabah babaci <4rabah@gmail.com>
2020-05-01 21:09:41 -0700

    fix axiosWithAuth baseURL

--- friends/src/utils/axiosWithAuth.js
+++ friends/src/utils/axiosWithAuth.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 const axiosWithAuth = () =>
   axios.create({
-    // baseURL: 'http://localhost:5000/api',
-    baseURL: 'https://rabah-backend.herokuapp.com/api/',
+    baseURL: 'https://rabah-backend.herokuapp.com/api',
     headers: {
       Authorization: localStorage.getItem('token'),
     },
